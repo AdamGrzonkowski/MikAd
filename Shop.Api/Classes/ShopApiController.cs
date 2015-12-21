b47475a52a28399f5d196b900b165165538a825c
mikolajtr <mikolajtr2@wp.pl>
2015-12-21 10:44:03 +0100

    dodana klasa bazowa kontrolerów API z podstawowymi metodami

---
+++ Shop.Api/Classes/ShopApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Shop.Repository.Interfaces;
+
+namespace Shop.Api.Classes
+{
+    public class ShopApiController<TEntity, TKey> : ApiController where TEntity : class
+    {
+        protected readonly IRepository<TEntity, TKey> repository;
+
+        public ShopApiController(IRepository<TEntity, TKey> repository)
+        {
+            this.repository = repository;
+        }
+
+        public TEntity Get(TKey id)
+        {
+            return repository.Get(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return repository.GetAll();
+        } 
+    }
+}
