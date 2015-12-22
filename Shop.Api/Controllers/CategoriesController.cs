5c4deb5e08b4b66e5da71df13607b32289025a78
mikolajtr <mikolajtr2@wp.pl>
2015-12-22 14:11:17 +0100

    wycofanie się z używania Unity

---
+++ Shop.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Api.Classes;
+using Shop.Model.Models;
+using Shop.Repository.Interfaces;
+
+namespace Shop.Api.Controllers
+{
+    public class CategoriesController : PublicRepositoryApiController<Category, int>
+    {
+        public CategoriesController(IRepository<Category, int> repository) : base(repository)
+        {
+        }
+    }
+}
---
+++ Shop.Api/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using Shop.Api.Classes;
+using Shop.Model.Models;
+using Shop.Repository.Interfaces;
+
+namespace Shop.Api.Controllers
+{
+    public class OrdersController : AuthorizedApiController<Order, int>
+    {
+        public OrdersController(IRepository<Order, int> repository) : base(repository)
+        {
+        }
+    }
+}
