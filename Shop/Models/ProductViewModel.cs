596ec7526a81e58590cc0ccdea89ef5814dcec18
mikolajtr <mikolajtr2@wp.pl>
2016-01-03 20:17:51 +0100

    poprawiony (nie całkiem) formularz dodawania produktów oraz strona podglądu

---
+++ Shop/Models/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class ProductViewModel
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+
+
+    }
+}
