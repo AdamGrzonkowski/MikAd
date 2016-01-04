793d92acc925dcad5dec7f74cfa49d2d491fe568
ADAM <245594@stud.umk.pl>
2016-01-04 18:29:48 +0100

    Stworzenie roli i użytkownika admin.

--- Shop/App_Start/BundleConfig.cs
+++ Shop/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Optimization;
+﻿using System.Data.Entity;
+using System.Web.Optimization;
 
 namespace Shop
 {
@@ -10,6 +11,7 @@ namespace Shop
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
