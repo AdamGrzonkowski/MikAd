785ad5507f228984439b8712d0ce0e5b02196376
ADAM <245594@stud.umk.pl>
2016-01-10 20:50:17 +0100

    Obsługa formularza kontaktowego - działa.

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace Shop.Controllers
 {
@@ -19,8 +20,24 @@ namespace Shop.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            
+            
             return View();
+        }
+
+        public ActionResult SubmitContactForm(string msgTitle, string msgEmailAddress, string msgBody)
+        {
+            var msg = new MailMessage();
+            msg.To.Add("Sklep_MVC@wp.pl");
+
+            msg.Subject = msgTitle;
+            msg.Body = "Wiadomość od: <a href='mailto:" + msgEmailAddress + "'>" + msgEmailAddress + "</a> <h4> Treść: </h4>" + msgBody;
+            msg.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient();
+            smtpClient.Send(msg);
+
+            return RedirectToAction("Contact");
         }
 
     }
